257225a553c780ff3fb98fd7885d94a0e5f46e11
JasperFinn <87607866+JasperFinn@users.noreply.github.com>
2025-02-25 21:37:21 +0100

    Map update

---
+++ mapa/geo_dane/point.js
@@ -0,0 +1,16 @@
+var point = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -6.542968749999403,
+            49.4395569589405
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
